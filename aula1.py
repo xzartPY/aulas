0c305fa9e4cf28470fc537db4854147e76375e73
xzartPY <34063878+xzartPY@users.noreply.github.com>
2017-11-28 12:07:27 -0200

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,25 +1,31 @@
-var_ = raw_input('Digite seu nome: ')
+while True:
+            var_ = raw_input('Digite seu nome: ')
                  
-var_1 = raw_input('Qual comida voce gosta?: ')
+            var_1 = raw_input('Qual comida voce gosta?: ')
 
-print(var_ + ' gosta de ' + var_1)
+            print(var_ + ' gosta de ' + var_1)
 
-var_2 = raw_input('Gostaria de deixar outra pessoa responder? sim/nao: ')
-
-
-
-if var_2 == 'sim':
-                 nome = raw_input('Digite seu nome: ')
-
-                 comida = raw_input('Qual comida voce gosta?: ')
-
-                 print(nome + ' gosta de ' + comida)
-                 print('obrigado por usar ' + var_ + ", e " + nome)
-                           
+            var_2 = raw_input('Gostaria de deixar outra pessoa responder? sim/nao: ')
 
 
-else:
-                 print('obrigado por usar ' + var_)
+
+            if var_2 == 'sim':
+                             nome = raw_input('Digite seu nome: ')
+
+                             comida = raw_input('Qual comida voce gosta?: ')
+
+                             print(nome + ' gosta de ' + comida)
+                             print('obrigado por usar ' + var_ + ", e " + nome)
+                             retornar = raw_input('Gostaria de continuar? sim/nao:')
+                             if retornar == 'sim':
+                                 for i in range(0, 101):
+                                     print 'carregando ' , i ,'%'
+
+
+            else:
+                             print('obrigado por usar ' + var_)
+                             break;
+
                  
 
     
